c407f4db845ead1298588197cfa6701ce7a52ff7
Birendra Acharya <biachary@microsoft.com>
2016-06-08 13:04:49 -0700

    Make user agent string passed to VSTS REST API accessible to consumers.

--- Microsoft.Alm.Authentication/Global.cs
+++ Microsoft.Alm.Authentication/Global.cs
@@ -2,7 +2,7 @@
 
 namespace Microsoft.Alm.Authentication
 {
-    internal static class Global
+    public static class Global
     {
         private static string _useragent = null;
 
@@ -10,12 +10,13 @@ namespace Microsoft.Alm.Authentication
         /// Creates the correct user-agent string for HTTP calls.
         /// </summary>
         /// <returns>The `user-agent` string for "git-tools".</returns>
-        public static string GetUserAgent()
+        public static string GetUserAgent(string defaultAgent = "CodeFlow")
         {
             if (_useragent == null)
             {
                 Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
-                _useragent = String.Format("CodeFlow ({0}; {1}; {2}) CLR/{3} Review-tools/{4}",
+                _useragent = String.Format("{0} ({1}; {2}; {3}) CLR/{4} Review-tools/{5}",
+                                           defaultAgent,
                                            Environment.OSVersion.VersionString,
                                            Environment.OSVersion.Platform,
                                            Environment.Is64BitOperatingSystem ? "x64" : "x86",
